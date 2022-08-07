06e4c86517c9c2a7fe560a2405b073d09e67adc9
Rongcui Dong <rongcuid@outlook.com>
2022-08-07 15:42:21 -0400

    Stub

--- src/core/post.rs
+++ src/core/post.rs
@@ -3,26 +3,63 @@ use eyre::*;
 use deadpool_sqlite::Connection;
 use time::OffsetDateTime;
 
-use super::{session::Session, user::User};
+use crate::error::to_eyre;
+
+use super::{from_row::FromRow, session::Session, topic::Topic, user::User};
 
 pub struct Post {
-    id: i64,
-    topic_id: i64,
-    author: User,
-    body: String,
-    post_number: i64,
-    public: bool,
-    created_at: OffsetDateTime,
-    updated_at: Option<OffsetDateTime>,
-    deleted_at: Option<OffsetDateTime>,
-    last_updated_by: Option<User>,
+    pub id: i64,
+    pub topic_id: i64,
+    pub author_user_id: i64,
+    pub body: String,
+    pub post_number: i64,
+    pub public: bool,
+    pub created_at: OffsetDateTime,
+    pub updated_at: Option<OffsetDateTime>,
+    pub deleted_at: Option<OffsetDateTime>,
+    pub last_updated_by: Option<i64>,
 }
 
 impl Post {
-    /// Queries a topic for a certain role
-    pub async fn query(_db: &Connection, _session: &Session, _id: i64) -> Result<Post> {
+    pub async fn query_by_topic_id(db: &Connection, session: &Session, topic_id: i64) -> Result<Vec<Post>> {
         todo!()
+    }
+    /// Checks visibility of post, but not the topic it belongs to
+    fn is_visible_to(&self, session: &Session) -> bool {
+        if self.deleted_at.is_some() {
+            // Deleted topic is only visible to admin and moderator
+            session.is_admin() || session.is_moderator()
+        } else if !self.public {
+            // Hidden post is only visible to admin, moderator, and topic author
+            session.is_admin()
+                || session.is_moderator()
+                || session.user_id() == Some(self.author_user_id)
+        } else {
+            // Public post is visible to everyone
+            true
+        }
     }
 }
 
-impl Post {}
+impl Post {
+    pub async fn topic(&self, db: &Connection, session: &Session) -> Result<Option<Topic>> {
+        Topic::query(db, session, self.topic_id).await
+    }
+}
+
+impl FromRow for Post {
+    fn try_from_row(row: &rusqlite::Row) -> Result<Self, rusqlite::Error> {
+        Ok(Self {
+            id: row.get("id")?,
+            topic_id: row.get("topic_id")?,
+            author_user_id: row.get("author_user_id")?,
+            post_number: row.get("post_number")?,
+            public: row.get("public")?,
+            created_at: row.get("created_at")?,
+            updated_at: row.get("updated_at")?,
+            deleted_at: row.get("deleted_at")?,
+            last_updated_by: row.get("last_updated_by")?,
+            body: row.get("body")?,
+        })
+    }
+}
--- src/core/topic.rs
+++ src/core/topic.rs
@@ -2,6 +2,7 @@ use eyre::*;
 
 use deadpool_sqlite::Connection;
 
+use rusqlite::OptionalExtension;
 use time::OffsetDateTime;
 
 use crate::error::to_eyre;
@@ -32,24 +33,52 @@ impl Topic {
                     [id],
                     Topic::try_from_row,
                 )
+                .optional()
             })
             .await
             .map_err(to_eyre)??;
-        if topic.is_visible_to(session) {
-            Ok(Some(topic))
+        Ok(topic.filter(|t| t.is_visible_to(session)))
+    }
+    pub async fn query_visibility(db: &Connection, session: &Session, id: i64) -> Result<bool> {
+        if let Some((author_user_id, public, deleted_at)) = db
+            .interact(move |conn| {
+                conn.query_row(
+                    r#"SELECT author_user_id, public, deleted_at FROM topics WHERE id = ?"#,
+                    [id],
+                    |row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
+                )
+                .optional()
+            })
+            .await
+            .map_err(to_eyre)??
+        {
+            Ok(Self::topic_is_visible_to(
+                author_user_id,
+                public,
+                &deleted_at,
+                session,
+            ))
         } else {
-            Ok(None)
+            Ok(false)
         }
     }
     fn is_visible_to(&self, session: &Session) -> bool {
-        if self.deleted_at.is_some() {
+        Self::topic_is_visible_to(self.author_user_id, self.public, &self.deleted_at, session)
+    }
+    fn topic_is_visible_to(
+        author_user_id: i64,
+        public: bool,
+        deleted_at: &Option<OffsetDateTime>,
+        session: &Session,
+    ) -> bool {
+        if deleted_at.is_some() {
             // Deleted topic is only visible to admin and moderator
             session.is_admin() || session.is_moderator()
-        } else if !self.public {
+        } else if !public {
             // Hidden post is only visible to admin, moderator, and topic author
             session.is_admin()
                 || session.is_moderator()
-                || session.user_id() == Some(self.author_user_id)
+                || session.user_id() == Some(author_user_id)
         } else {
             // Public post is visible to everyone
             true
